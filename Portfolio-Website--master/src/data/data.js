753cc3583eb168b6f641c0217336bd0224449b75
arierhiprecious@gmail.com <arierhiprecious@gmail.com>
2023-01-02 05:40:43 -0800

    'portfolio_v_1'

---
+++ Portfolio-Website--master/src/data/data.js
@@ -0,0 +1,155 @@
+import { AiOutlineHome, AiOutlineCalculator, AiOutlineSetting, AiOutlineShareAlt } from "react-icons/ai"
+import { RiUserLine } from "react-icons/ri"
+import { FaRegLightbulb, FaFacebookF, FaTwitter, FaInstagram, FaDribbble, FaShippingFast } from "react-icons/fa"
+import { CgCardClubs } from "react-icons/cg"
+import { TbMailOpened } from "react-icons/tb"
+import { GoMail } from "react-icons/go"
+import { GiChart } from "react-icons/gi"
+import { FiLayers } from "react-icons/fi"
+import { BsPhone } from "react-icons/bs"
+import { BsPhoneVibrate, BsVectorPen } from "react-icons/bs"
+import { MdShareLocation, MdLocalShipping, MdOutlineLocalShipping } from "react-icons/md"
+
+export const menu = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+    icon: <AiOutlineHome />,
+  },
+  {
+    id: 2,
+    text: "resume",
+    url: "/resume",
+    icon: <RiUserLine />,
+  },
+  {
+    id: 3,
+    text: "portfolio",
+    url: "/portfolio",
+    icon: <FaRegLightbulb />,
+  },
+  {
+    id: 4,
+    text: "blog",
+    url: "/blog",
+    icon: <CgCardClubs />,
+  },
+  {
+    id: 5,
+    text: "contact",
+    url: "/contact",
+    icon: <TbMailOpened />,
+  },
+]
+
+export const socialIcon = [
+  {
+    id: 1,
+    icon: <FaFacebookF />,
+    class: "facebook",
+  },
+  {
+    id: 2,
+    icon: <FaTwitter />,
+    class: "twitter",
+  },
+  
+]
+
+export const side = [
+  {
+    id: 1,
+    num: 75,
+    text: "Backend Development",
+    class: "skill1",
+  },
+  {
+    id: 2,
+    num: 75,
+    text: "Frontend Development",
+    class: "skill2",
+  },
+  {
+    id: 3,
+    num: 75,
+    text: "App Development",
+    class: "skill3",
+  },
+]
+
+export const about = [
+  {
+    deatils: [
+      {
+        text: "My Name",
+        value: "Arierhi Precious",
+        icon: <RiUserLine />,
+      },
+      { text: "My Age", value: "25 Years 333 Days", icon: <AiOutlineCalculator /> },
+      { text: "Email Address", value: "arierhiprecious@gmail.com", icon: <GoMail /> },
+      { text: "Phone Number", value: "+2349071991647", icon: <BsPhoneVibrate /> },
+      { text: "Address", value: "Warri,Delta state.", icon: <MdShareLocation /> },
+    ],
+    bio: [
+      {
+        para1: "My name is Arierhi Precious,I'm a Jr software developer at Eazyconnect. ",
+        para2: "I work at a startup called EazyConnect.My role at Eazytech includes building mobile and web applications for client,building applications that solves societal problems.",
+        para3: "My recent achievement include cloning a crypto trading app called Coinbase. i would like to own a Robotics firm few years from now",
+        images: "./images/bio.png",
+      },
+    ],
+
+    serives: [
+      {
+        id: 1,
+        icon: <AiOutlineSetting />,
+        title: "Web Development",
+        text: "from scratch",
+        decs: "i build scalable website and web application to meet your business needs",
+      },
+      {
+        id: 2,
+        icon: <BsPhone />,
+        title: "Mobile Development",
+        text: "stand alone",
+        decs: "I build cross platform mobile application to solve your business needs",
+      }
+    ],
+
+    review: [
+      {
+        id: 1,
+        desc: "Amazing product i will say. He delivered a product that automate accounting and marketing in my small business. ",
+        name: "Mr Edward",
+        link: "Favour building enterprice",
+        cover: "./images/aut1.jpg",
+      },
+     
+    ],
+
+    price: [
+      {
+        id: 1,
+        icon: <MdOutlineLocalShipping />,
+        title: "WEBSITE/WEB APPLICATION",
+        desc: "Suitable for small business owners.",
+        pri: "200.00",
+      },
+      {
+        id: 2,
+        icon: <MdLocalShipping />,
+        title: "ANDROID/IOS APP",
+        desc: "Suitable for small business or Organizations.",
+        pri: "700",
+      },
+      {
+        id: 3,
+        icon: <FaShippingFast />,
+        title: "PRO PACKAGE",
+        desc: "Suitable for small business or Organizations.",
+        pri: "97.00s",
+      },
+    ],
+  },
+]
---
+++ Portfolio-Website--master/src/pages/Pages.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Header } from "../common/Header"
+import { Home } from "../components/home/Home"
+import { Resume } from "../components/page/Resume"
+import { Portfolio } from "../components/page/Portfolio"
+import { Blog } from "../components/page/Blog"
+import { Contact } from "../components/page/Contact"
+import { SideContent } from "../components/side/SideContent"
+
+export const Pages = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <div className='main-div'>
+
+          <div className='side'>
+            <SideContent />
+          </div>
+          
+          <main>
+            <Header />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/resume' element={<Resume />} />
+              <Route path='/portfolio' element={<Portfolio />} />
+              <Route path='/blog' element={<Blog />} />
+              <Route path='/contact' element={<Contact />} />
+            </Routes>
+          </main>
+
+
+        </div>
+      </BrowserRouter>
+    </>
+  )
+}
